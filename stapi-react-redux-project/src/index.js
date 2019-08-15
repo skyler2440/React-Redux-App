432f37e0db0c717c36c2f0eee14c20155d3f8bed
skyler dowdy <skylerwebdev@gmail.com>
2019-08-15 15:00:30 -0400

    indexJS setup with store and redux dev tools

--- stapi-react-redux-project/src/index.js
+++ stapi-react-redux-project/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from 'redux';
 import { reducer } from "./reducers";
 import { Provider } from "react-redux";
-
-const store = createStore(reducer);
-
+import thunk from 'redux-thunk';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 ReactDOM.render(
   <Provider store={store}>
     <App />
